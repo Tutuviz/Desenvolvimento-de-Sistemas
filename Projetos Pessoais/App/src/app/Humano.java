ccb6c3810a2bb580de9d83be46a0e81d6292a006
Arthur Passos <54548671+Tutuviz@users.noreply.github.com>
2020-03-09 11:54:35 -0300

    Update Humano.java

--- Projetos Pessoais/App/src/app/Humano.java
+++ Projetos Pessoais/App/src/app/Humano.java
@@ -3,41 +3,70 @@ package app;
 import javax.swing.JOptionPane;
 
 public class Humano {
+    //Digitado
     private String name;
     private String sex;
+    private int age;
     private double height;
     private double peso;
-    private int age;
-    private int stamina;
     private boolean disease;
-    private boolean alive;
     private boolean oculos;
+    
+    //Existence
     private boolean exist;
+    private boolean alive;
+    
+    //Caracteristicas
+    private int stamina;
 
+    //Outros
+    String[] options = {"Sim", "Não"};
+    int esc;
+
+
+    //Main Menu
     public void preencher() {
-        int esc1;
         this.setName(JOptionPane.showInputDialog("Digite o nome do humano: "));
         this.setSex(JOptionPane.showInputDialog("Digite o sexo do humano: "));
         this.setHeight(Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do humano: (Em metros)")));
         this.setPeso(Double.parseDouble(JOptionPane.showInputDialog("Digite o peso do humano: (Em Quilos)")));
         this.setAge(Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do humano: ")));
-        esc1 = Integer.parseInt(JOptionPane.showInputDialog("O humano usa oculos? \n1 - sim\n2 - não"));
-        if (esc1 == 1){
+        esc = JOptionPane.showOptionDialog(null, "O humano usa oculos?", "Oculos", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        if (esc==0){
             this.setOculos(true);
         } else {
             this.setOculos(false);
         }
-        esc1 = Integer.parseInt(JOptionPane.showInputDialog("O humano tem uma doença? \n1 - sim\n2 - não"));
-        if (esc1 == 1){
+        esc = JOptionPane.showOptionDialog(null, "O humano tem doenças?", "Doenças", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        if (esc == 0){
             this.setDisease(true);
         } else {
             this.setDisease(false);
         }
+        this.setAlive(true);
         this.setExist(true);
     }
 
     public void status(){
         JOptionPane.showMessageDialog(null, "O nome é: "+this.getName()+"\nA idade é: "+this.getAge()+"\nO sexo é: "+this.getSex()+"\nA altura é: "+this.getHeight()+"\nO peso é: "+this.getPeso()+"\nUsa oculos? "+this.isOculos()+"\nTem doença?"+this.isDisease());
+    }
+
+    public void delete(){
+        int esc = JOptionPane.showOptionDialog(null, "Deseja realmente apagar para sempre (muito tempo) o/a "+this.getName(), "Deletar "+this.getName(), JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        if (esc == 0){
+            this.setAge(0);
+            this.setAlive(false);
+            this.setDisease(false);
+            this.setExist(false);
+            this.setHeight(0);
+            this.setName("Nenhum");
+            this.setOculos(false);
+            this.setPeso(0);
+            this.setSex(null);
+            this.setStamina(0);
+        } else {
+            JOptionPane.showMessageDialog(null, "O humano não foi deletado");
+        }
     }
 
     public void fatigue(){
@@ -53,6 +82,47 @@ public class Humano {
                 this.setDisease(true);
             }
         }
+    }
+
+    //Atividades
+    public void dormir(){
+        if (this.getStamina() == 100){
+            JOptionPane.showMessageDialog(null, this.getName()+" já está descansado o suficiente, faça outras atividades para gastar energia");
+        } else {
+            JOptionPane.showMessageDialog(null, this.getName()+" dormiu e restaurou toda a energia");
+            this.setStamina(100);
+            
+        }
+    }
+
+    public void estudar(){
+        JOptionPane.showMessageDialog(null, this.getName()+" estudou arduamente e se sente mais inteligente porém desgastado");
+        setStamina(getStamina()-10);
+    }
+
+    public void trabalhar(){
+        JOptionPane.showMessageDialog(null, this.getName()+" trabalhou muito e agora deu uma pausa");
+        setStamina(getStamina()-25);
+    }
+    public void jogar(){
+        JOptionPane.showMessageDialog(null, this.getName()+" jogou durante horas e consegue sentir uma melhoria no jogo");
+        setStamina(getStamina()-20);
+    }
+    public void comer(){
+        JOptionPane.showMessageDialog(null, this.getName()+" comeu bem e está cheio agora");
+        setStamina(getStamina()-5);
+    }
+    public void coitar(){
+        JOptionPane.showMessageDialog(null, this.getName()+" ( ͡° ͜ʖ ͡°) I know, you know");
+        setStamina(getStamina()-20);
+    }
+    public void passear(){
+        JOptionPane.showMessageDialog(null, this.getName()+" deu um passeio e aproveitou bem o dia");
+        setStamina(getStamina()-30);
+    }
+    public void exercitar(){
+        JOptionPane.showMessageDialog(null, this.getName()+" se exercitou e agora está muito exausto");
+        setStamina(getStamina()-40);
     }
 
     // Get and Set
@@ -139,7 +209,8 @@ public class Humano {
     // Constructor
     public Humano() {
         this.setStamina(100);
-        this.setAlive(true);
+        this.setExist(false);
+        this.setName("Nenhum");
     }
 
 
